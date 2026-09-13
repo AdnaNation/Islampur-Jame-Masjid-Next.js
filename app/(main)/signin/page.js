bf94405a2e915177511642740066618f9fbb04eb
AdnaNation <azizulislamadnan@gmail.com>
2026-09-13 13:22:19 +0600

    modified: user login

--- app/(main)/signin/page.js
+++ app/(main)/signin/page.js
@@ -1,18 +1,23 @@
+"use client";
 import Link from "next/link";
+import Swal from "sweetalert2";
 
 const SignIn = () => {
   const handleSingIn = () => {
     Swal.fire({
-      position: "top-end",
+      position: "center",
       icon: "error",
       title: "দুঃখিত! এই ফিচারটি এখনো চালু হয়নি।",
       showConfirmButton: false,
-      timer: 1500,
+      timer: 1200,
     });
   };
 
   return (
-    <form className=" mx-auto md:max-w-80 space-y-2 mt-14">
+    <form
+      onClick={handleSingIn}
+      className=" mx-auto md:max-w-80 space-y-2 mt-14"
+    >
       <label className="form-control md:w-96 w-72 px-1 mx-auto">
         <div className="label">
           <span className="label-text">আপনার নাম্বার কী?</span>
@@ -30,11 +35,7 @@ const SignIn = () => {
         </div>
       </label>
       <div className="text-center">
-        <button
-          onClick={handleSingIn}
-          className="btn btn-outline btn-info"
-          type="button"
-        >
+        <button className="btn btn-outline btn-info" type="button">
           প্রবেশ করুন
         </button>
       </div>
